5ec46b6f4b6890baba1506a79fdeff67d9ddecbc
klpabes <klpabes99@gmail.com>
2024-11-08 09:49:56 +0800

    add templates

--- tb_webapp/app/views.py
+++ tb_webapp/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'app/index.html')
 
 def register(request):
     pass
