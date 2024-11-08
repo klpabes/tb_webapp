bf5d5ee06de9a6774c7c3beec2d7dae39db0565e
klpabes <klpabes99@gmail.com>
2024-11-08 14:58:21 +0800

    add register

--- tb_webapp/app/views.py
+++ tb_webapp/app/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 def index(request):
     return render(request, 'app/index.html')
 
 def register(request):
-    pass
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('index')
+            
+    else:
+        form = UserRegisterForm()
+    return render(request, 'app/register.html', {'form': form})
 
 def login(request):
     pass
